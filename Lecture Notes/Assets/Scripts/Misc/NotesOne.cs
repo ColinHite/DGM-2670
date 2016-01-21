53c3bc77bd1139f14aa52ce513f72c14a81a8e5a
unknown <10712935@CS514-19.ad.uvu.edu>
2016-01-21 11:09:34 -0700

    Did some more notes on anthony's game. This time I learned how to make the mouse raycast be relative to the screen mouse pos. I learned that Lerp is basically a smooth trans from one place to another. Lastly I learned how and why a rigidbody fights a new update location.

--- Lecture Notes/Assets/Scripts/Misc/NotesOne.cs
+++ Lecture Notes/Assets/Scripts/Misc/NotesOne.cs
@@ -5,9 +5,20 @@ public class NotesOne : MonoBehaviour {
 
 	Rigidbody thisRigidBody;
 
+	public float speed = 100f;
+
 	void Start ()
 	{
 		thisRigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void OnMouseDrag()
+	{
+		//this moves the position but is not working with the rigidbody.
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		mousePosition.z = 0;
+		Vector3 currentPosition = Vector3.Lerp(transform.position, mousePosition, speed*Time.deltaTime);
+		thisRigidBody.MovePosition (currentPosition);
 	}
 
 	void OnMouseDown()
@@ -15,13 +26,13 @@ public class NotesOne : MonoBehaviour {
 		print ("Down");
 		thisRigidBody.AddForce (0, 1000, 0);
 	}
+
 	void OnMouseEnter()
 	{
 		print ("Enter");
-	}void OnMouseDrag()
-	{
-		print ("Drag");
-	}void OnMouseExit()
+	}
+
+	void OnMouseExit()
 	{
 		print ("Exit");
 	}
