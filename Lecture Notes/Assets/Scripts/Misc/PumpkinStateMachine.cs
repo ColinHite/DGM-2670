a21c2d93c2f69decbc3a47ff783bc641ffa3b9b3
unknown <10712935@CS514-14.ad.uvu.edu>
2016-01-26 11:19:10 -0700

    Took some more notes on the game. Also we learned how to change the rendered material/ Sprite in game.

---
+++ Lecture Notes/Assets/Scripts/Misc/PumpkinStateMachine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PumpkinStateMachine : MonoBehaviour {
+
+	public PumpkinControler pumpkinController;
+	private MeshRenderer thisMaterial;
+
+	public StaticPumpkins.AvalibleStates thisPumpkinsState;
+
+	void ChangeState (int _i)
+	{
+		thisMaterial = GetComponent<MeshRenderer> ();
+		thisMaterial.material = pumpkinController.pumpkinMaterial[_i];
+
+//		print (pumpkinController.pumpkinMaterial [_i]);
+//		print (_i)
+
+	}
+
+	void SwitchState ()
+	{
+		switch (thisPumpkinsState) {
+		case StaticPumpkins.AvalibleStates.Standard:
+			ChangeState (0);
+			break;
+		case StaticPumpkins.AvalibleStates.Ice:
+			ChangeState (1);
+			break;
+		case StaticPumpkins.AvalibleStates.Fire:
+			ChangeState (2);
+			break;
+		case StaticPumpkins.AvalibleStates.Nuke:
+			ChangeState (3);
+			break;
+		}
+	}
+
+
+		void Start ()
+	{
+
+		SwitchState ();
+
+	}
+}
---
+++ Lecture Notes/Assets/Scripts/StaticPumpkins.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticPumpkins : MonoBehaviour {
+
+	//This Changes the state (abilities and attributes) of the pumpkins.
+	public enum AvalibleStates {
+		
+		Standard,
+		Ice,
+		Fire,
+		Nuke
+		
+	}
+	
+	public static AvalibleStates currentState = AvalibleStates.Standard;
+
+}
