366267547a381e969ce0c541b92e35c2ba9f0388
unknown <10712935@CS514-22.ad.uvu.edu>
2016-02-04 11:24:00 -0700

    Learned how to make an inventory! Yes!

---
+++ Lecture Notes/Assets/Scripts/Powerups/WeaponClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class WeaponClass : MonoBehaviour {
+
+	public static Action<WeaponClass> PassWeapon;
+
+	void OnTriggerEnter()
+	{
+		print (this);
+		if (PassWeapon != null)
+			PassWeapon (this);
+
+		gameObject.SetActive (false);
+	}
+}
---
+++ Lecture Notes/Assets/Scripts/Powerups/WeaponInventory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WeaponInventory : MonoBehaviour {
+
+	public List<WeaponClass> weapons;
+
+	void AddToInventory (WeaponClass _weapon) 
+	{
+		print (_weapon.name);
+		weapons.Add (_weapon);
+	}
+
+	void Start () {
+		weapons = new List<WeaponClass> ();
+		WeaponClass.PassWeapon += AddToInventory;
+	}
+
+}
