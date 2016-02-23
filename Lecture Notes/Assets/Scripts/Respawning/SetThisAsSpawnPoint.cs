980e0fb4c2e6863a6837c887e2c09fce6ad5c20a
unknown <10712935@CS514-09.ad.uvu.edu>
2016-02-23 10:53:55 -0700

    Got some notes from class on a respawn system. Missed a bit remeber to check out anthony's git hub thing!

---
+++ Lecture Notes/Assets/Scripts/Respawning/SetThisAsSpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SetThisAsSpawnPoint : MonoBehaviour {
+
+	public static Action<Transform> PassTransformAsSpawnPoint;
+
+	// Use this for initialization
+	void Start () 
+	{
+	if (PassTransformAsSpawnPoint != null)
+			PassTransformAsSpawnPoint (transform);
+	}
+	
+}
---
+++ Lecture Notes/Assets/Scripts/Respawning/SpawnControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnControl : MonoBehaviour {
+	
+	public MeshRenderer meshRenderer;
+	public List<Transform> spawnPoints;
+
+	void AddToSpawnPointsList (Transform _spawnPoint)
+	{
+
+		spawnPoints.Add (_spawnPoint);
+
+	}
+
+	void Start ()
+	{
+		spawnPoints = new List<Transform> ();
+		SetThisAsSpawnPoint.PassTransformAsSpawnPoint += AddToSpawnPointsList;
+
+	}
+
+	IEnumerator Spawn ()
+	{
+		yield return new WaitForSeconds (2);
+		meshRenderer.enabled = true;
+
+		int randomSpawnPointNum = Random.Range (0, spawnPoints.Count - 1);
+
+		transform.position = spawnPoints[randomSpawnPointNum].position;
+
+	}
+
+	void OnMouseDown ()
+	{
+		meshRenderer.enabled = false;
+		StartCoroutine (Spawn ());
+	}
+
+}
