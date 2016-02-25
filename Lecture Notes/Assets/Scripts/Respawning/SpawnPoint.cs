7f761353e9bb5ad5d8f7030ca09b5944241e6499
unknown <10712935@CS514-15.ad.uvu.edu>
2016-02-25 11:21:41 -0700

    Started getting an AI system ready! Exciting ya bro?!

---
+++ Lecture Notes/Assets/Scripts/Respawning/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnPoint : MonoBehaviour {
+
+	public List<Transform> spawnPoints;
+
+	public void AddToSpawnPointsList (Transform _spawnPoint)
+	{
+		spawnPoints.Add (_spawnPoint);
+	}
+
+	void Start () 
+	{
+		spawnPoints = new List<Transform> ();
+		SetThisAsSpawnPoint.PassTransformAsSpawnPoint += AddToSpawnPointsList;
+	}
+}
