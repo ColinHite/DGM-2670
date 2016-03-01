c8b8a260ad229b994646361014e370dde6e9e14c
unknown <10712935@CS514-21.ad.uvu.edu>
2016-03-01 11:20:25 -0700

    Started to learn how to build puzzles and import from maya also dem animations y'all.

---
+++ Lecture Notes/Assets/Scripts/Puzel/ActivatePuzzel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivatePuzzel : MonoBehaviour {
+
+	public Animator anims;
+
+	public enum colors [green,red,blue]
+
+	void OnTriggerEnter ()
+	{
+
+		anims.SetBool ("CanOpen", true);
+
+	}
+}
---
+++ Lecture Notes/Assets/Scripts/Puzel/PuzzelControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzelControl : MonoBehaviour {
+
+	public ActivatePuzzel[] puzzleParts;
+
+
+	public void Solve()
+	{
+
+
+
+	}
+
+}
