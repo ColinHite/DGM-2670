94ebc16b382d7acb13b7c8eff9976abc596f62d4
unknown <10712935@CS514-14.ad.uvu.edu>
2016-03-31 11:15:33 -0600

    Started to try and get the plane to follow the mouse. Got problems going so ill try something in time.

--- Comp_Hite/Assets/Scripts/PlayerCont.cs
+++ Comp_Hite/Assets/Scripts/PlayerCont.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class PlayerCont : MonoBehaviour {
 
+    //Air Plane pos / mouse pos
+    public Transform airPlanePos;
+    //public Transform mousePos;
+
+    //AirPlane speed controls
 	public float speed =5;
 	private float playerSpeed;
 
-	void Start () 
-	{
+	void Start ()
+    { 
 		playerSpeed = GetComponent<Rigidbody> ().velocity.x;
 	}
 
 	void Update () 
 	{
 
-		GetComponent<Rigidbody>().velocity = new Vector3 (playerSpeed, 0, speed);
+        transform.localPosition = Vector3.Lerp (airPlanePos.position, Input.mousePosition, Time.time);
+
+        GetComponent<Rigidbody>().velocity = new Vector3 (playerSpeed, 0, speed);
 	}
 }
