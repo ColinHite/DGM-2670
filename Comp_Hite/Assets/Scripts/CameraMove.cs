3ea0f1400728fa9c957436123eb246e3d9e1737c
Colin H <colin.hite@gmail.com>
2016-04-04 23:34:44 -0600

    Found a great way to make the plane move and I might translate that over.

--- Comp_Hite/Assets/Scripts/CameraMove.cs
+++ Comp_Hite/Assets/Scripts/CameraMove.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class CameraMove : MonoBehaviour {
 
-    public static Quaternion targetRot;
-    public static float RotX;
-    public static float RotY;
-    public static float RotZ;
+    //public static Quaternion targetRot;
+   // public static float RotX;
+    //public static float RotY;
+   // public static float RotZ;
 
     public static Vector3 targetPos;
 	public static float ViewX;
@@ -35,9 +35,9 @@ public class CameraMove : MonoBehaviour {
 
 		targetPos = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
 
-		ViewX -= (ViewX - targetPos.x) * 0.1f;
-        ViewY -= (ViewY - targetPos.y) * 0.1f;
-        ViewZ -= (ViewZ - targetPos.z) * 0.1f;
+		ViewX -= (ViewX - targetPos.x) * 1f;
+        ViewY -= (ViewY - targetPos.y) * 1f;
+        ViewZ -= (ViewZ - targetPos.z) * 1f;
 
 		gameObject.transform.position = new Vector3(ViewX, ViewY, ViewZ);
 
---
+++ Comp_Hite/Assets/Scripts/PlaneFly.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneFly : MonoBehaviour {
+
+    public float speed = 5.0f;
+
+
+    void Start ()
+    {
+
+    }
+	
+
+	void Update ()
+    {
+        transform.Rotate(-Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
